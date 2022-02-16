d843e67786d546acac9b2c1643f303258340d210
zolvo <asicomindo@gmail.com>
2022-02-15 19:40:08 -0600

    wordle first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const tileDisplay = document.querySelector(".tile-container");
+const keyboard = document.querySelector(".key-container");
+
+const keys = [
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "0",
+  "P",
+  "A",
+  "s",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "ENTER",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+  "« ",
+];
+
+const handleClick = () => {
+  console.log("clicked");
+};
+
+keys.forEach((key) => {
+  const buttonElement = document.createElement("button");
+  buttonElement.textContent = key;
+  buttonElement.setAttribute("id", key);
+  buttonElement.addEventListener("click", handleClick);
+  keyboard.append(buttonElement);
+});
